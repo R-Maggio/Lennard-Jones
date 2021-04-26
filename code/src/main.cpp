0ded98275c2dfb9739006876b38ee836023291bd
R-Maggio <raphael.maggio.aprile@gmail.com>
2021-04-26 14:50:07 +0200

    update gitignore

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,8 +7,8 @@ int main(int, char**) {
     std::string pathFolder = "../../printParticles/data/";
     std::string fileName = "_particles.csv";
 
-    LJSimulation simulation = LJSimulation(1., 1., 1., 10., 10., 100);
-    simulation.placeRandomParticles(1000);
+    LJSimulation simulation = LJSimulation(0.0001, 0.00001, 0.0001, 10., 10., 10);
+    simulation.placeRandomParticles(10);
     for (int i = 0; i < 100; i++)
     {
         simulation.computeStep(0.01);
